e7a2f5b6ef1b7fe3481da6ac8472e720aec54503
adrean paul sorono <adreanpaulsorono@gmail.com>
2025-06-28 19:37:09 +0800

    Add test navbar Updated app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./route/Home";
+import Chat from "./route/Chat";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<div>Home</div>} />
-      <Route path="/chat" element={<div>Chat</div>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/chat" element={<Chat />} />
     </Routes>
   );
 }
