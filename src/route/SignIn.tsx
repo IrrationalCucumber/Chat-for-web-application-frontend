9a90aea4bf1f7e41e9173a736c93a97608f61640
adrean paul sorono <adreanpaulsorono@gmail.com>
2025-10-01 12:06:20 +0800

    Initial SignIn module

--- src/route/SignIn.tsx
+++ src/route/SignIn.tsx
@@ -1,3 +1,47 @@
+import * as React from "react";
+import axios from "axios";
+
 export default function SignIn() {
-  <>Sign In</>;
+  const [userCred, setUserCred] = React.useState({
+    username: "",
+    password: "",
+  });
+  const apiURL = import.meta.env.VITE_URL; //add api for backend in env file
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (userCred.username != "" || userCred.password != "") {
+      try {
+        alert("check");
+        axios.post(`${apiURL}/sign-in`);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  return (
+    <>
+      Sign In
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          placeholder="Username...."
+          name="username"
+          value={userCred.username}
+          onChange={(e) =>
+            setUserCred({ ...userCred, username: e.target.value })
+          }
+        />
+        <input
+          placeholder="Password..."
+          name="password"
+          value={userCred.password}
+          onChange={(e) =>
+            setUserCred({ ...userCred, password: e.target.value })
+          }
+        />
+        <button type="submit"></button>
+      </form>
+    </>
+  );
 }
